c04169344576f3400cb381027cf663c0dfc51ce2
fat-jul <julien.fatnassi@gmail.com>
2025-11-15 05:58:06 +0100

    Front-end first draft

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 def home_view(request):
-    return render(request, 'core/index.html' )
+    breadcumbs  = [
+        ("Page d'accueil", reverse('index'))
+        ]
+    
+    return render(request, 'core/index.html', {'breadcumbs' : breadcumbs}  )
